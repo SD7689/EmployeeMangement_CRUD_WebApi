20892a7b57bf0d4c51520d0458ad64cc8dea5bb7
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-03-26 13:48:03 +0530

    [Shivam] Add. added a Folder and created IRepo Interface.

---
+++ Repository/IRepos/IRepo.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IRepo.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Repository.IRepos
+{
+    using Model;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// interface IRepo.
+    /// </summary>
+    public interface IRepo
+    {
+        /// <summary>
+        /// GetEmployee method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Employee GetEmployee(int id);
+
+        /// <summary>
+        /// GetAllEmployees method.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Employee> GetAllEmployees();
+
+        /// <summary>
+        /// AddEmployee method.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        Employee AddEmployee(Employee employee);
+
+        /// <summary>
+        /// UpdateEmployee method.
+        /// </summary>
+        /// <param name="employeeChanges"></param>
+        /// <returns></returns>
+        Employee UpdateEmployee(Employee employeeChanges);
+
+        /// <summary>
+        /// DeleteEmployee method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Employee DeleteEmployee(int id);
+    }
+}
