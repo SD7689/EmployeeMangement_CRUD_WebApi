1731d96a2945217797ef6790c6086d694b255503
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-03-29 13:33:05 +0530

    [Shivam] Refactor. CRUD Operation Implemented with StyleCops

--- Manager/ManagerInterface/IEmployeeManager.cs
+++ Manager/ManagerInterface/IEmployeeManager.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IManager.cs" company="Bridgelabz">
+// <copyright file="IEmployeeManager.cs" company="Bridgelabz">
 // Copyright © 2020 Company="BridgeLabz".
 // </copyright>
 // <creator name="Shivam Dewangan"/>
@@ -8,21 +8,46 @@
 namespace Manager.ManagerInterface
 {
     using Model;
-    using System;
     using System.Collections.Generic;
-    using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// IEmployeeManager Interface. 
+    /// </summary>
     public interface IEmployeeManager
     {
+        /// <summary>
+        /// GetEmployee method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> It returns if execution is successful or not </returns>
         Employee GetEmployee(int id);
 
+        /// <summary>
+        /// GetAllEmployees Method.
+        /// </summary>
+        /// <returns> It returns if execution is successful or not </returns>
         IEnumerable<Employee> GetAllEmployees();
-        
+
+        /// <summary>
+        /// AddEmployee method.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns> It returns if execution is successful or not </returns>
         Task<int> AddEmployee(Employee employee);
-        
+
+        /// <summary>
+        /// UpdateEmployee Method.
+        /// </summary>
+        /// <param name="employeeChanges"></param>
+        /// <returns> It returns if execution is successful or not </returns>
         Task<int> UpdateEmployee(Employee employeeChanges);
-        
+
+        /// <summary>
+        /// DeleteEmployee Method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> It returns if execution is successful or not </returns>
         Employee DeleteEmployee(int id);
     }
 }
