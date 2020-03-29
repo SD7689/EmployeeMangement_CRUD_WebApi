218527dda5db64f2d0e498ea2c743e1e1581648b
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-03-29 13:35:05 +0530

    [Shivam] Refactor. CRUD Operation Implemented with StyleCops

--- Repository/Program.cs
+++ Repository/Program.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 
 namespace Repository
 {
+    /// <summary>
+    /// Program Class.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main Method.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
