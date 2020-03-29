3a4537e59b749f1cbbaaa76b1c767a53938e783d
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-03-29 13:32:57 +0530

    [Shivam] Refactor. CRUD Operation Implemented with StyleCops

--- Manager/ManagerClass/EmployeeManager.cs
+++ Manager/ManagerClass/EmployeeManager.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ImpManager.cs" company="Bridgelabz">
+// <copyright file="EmployeeManager.cs" company="Bridgelabz">
 // Copyright © 2020 Company="BridgeLabz".
 // </copyright>
 // <creator name="Shivam Dewangan"/>
@@ -10,41 +10,72 @@ namespace Manager.ManagerImplementation
     using Manager.ManagerInterface;
     using Model;
     using Repository.RepositoryInterface;
-    using System;
     using System.Collections.Generic;
-    using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class EmployeeManager : IEmployeeManager
     {
+        /// <summary>
+        /// The user IEmployeeRepository repo
+        /// </summary>
         private readonly IEmployeeRepository repo;
 
+        /// <summary>
+        /// EmployeeManager Constructor
+        /// </summary>
+        /// <param name="repo"></param>
         public EmployeeManager(IEmployeeRepository repo)
         {
             this.repo = repo;
         }
 
+        /// <summary>
+        /// AddEmployee Method.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns> Add Employee data in database </returns>
         public Task<int> AddEmployee(Employee employee)
         {
             return this.repo.AddEmployee(employee);
         }
 
+        /// <summary>
+        /// DeleteEmployee Method
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Delte Employee data in database</returns>
         public Employee DeleteEmployee(int id)
         {
             return this.repo.DeleteEmployee(id);
         }
 
+        /// <summary>
+        /// GetAllEmployees Method.
+        /// </summary>
+        /// <returns> GetAllEmployees Employee data in database </returns>
         public IEnumerable<Employee> GetAllEmployees()
         {
             return this.repo.GetAllEmployees();
         }
 
+        /// <summary>
+        /// GetEmployee Method.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> GetEmployee data in database</returns>
         public Employee GetEmployee(int id)
         {
             return this.repo.GetEmployee(id);
         }
 
-
+        /// <summary>
+        /// UpdateEmployee Method.
+        /// </summary>
+        /// <param name="employeeChanges"></param>
+        /// <returns> UpdateEmployee Employee data in database </returns>
         public Task<int> UpdateEmployee(Employee employeeChanges)
         {
             return this.repo.UpdateEmployee(employeeChanges);
