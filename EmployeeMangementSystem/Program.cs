e7d157238c23e9ff924680ab8e10e9e91b218a76
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-03-29 13:24:49 +0530

    [Shivam] Refactor. CRUD Operation Implemented with StyleCops

--- EmployeeMangementSystem/Program.cs
+++ EmployeeMangementSystem/Program.cs
@@ -9,13 +9,25 @@ namespace EmployeeMangementCurd_Api
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
 
+    /// <summary>
+    /// Program Class.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main Method.
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// CreateWebHostBuilder Method.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
