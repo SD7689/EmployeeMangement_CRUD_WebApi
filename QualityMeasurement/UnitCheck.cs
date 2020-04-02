e07fb7c4de90c47cc714f12a997b2cbdddc6a9f1
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-02 18:23:01 +0530

    [Shivam] Add. added UnitCheck.cs

---
+++ QualityMeasurement/UnitCheck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+    public class UnitCheck
+    {
+        public string type;
+        public int Length;
+
+        public UnitCheck()
+        {
+
+        }
+
+        public UnitCheck(string type , int Length)
+        {
+            this.type = type;
+            this.Length = Length;
+        }
+
+        public UnitCheck(string type)
+        {
+            this.type = type;
+        }
+
+        public int EqualsValue()
+        {
+            return this.Length;
+        }
+
+        public int ConvertFeetToInch()
+        {
+            return this.Length * 12;
+        }
+        public double ConvertInchToFeet()
+        {
+            return this.Length / 12;
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+
+    }
+}
