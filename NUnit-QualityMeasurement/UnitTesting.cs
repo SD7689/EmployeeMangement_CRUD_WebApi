ca4c736e76d250dcecf8dc4cd9956fec21ece8da
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-02 18:18:32 +0530

    [Shivam] Refactor. TC-1.14 Compare 1Feet==1Inch Done

--- NUnit-QualityMeasurement/UnitTesting.cs
+++ NUnit-QualityMeasurement/UnitTesting.cs
@@ -106,17 +106,36 @@ namespace NUnit_QualityMeasurement
         [Test]
         public void Compare_1ft_Not_Equal_to_1inch_return_False()
         {
-            UnitCheck inch = new UnitCheck("Inch",1);
-            double actual = inch.ConvertInchToFeet();
-            Assert.AreNotEqual(1,actual);
+            UnitCheck feet = new UnitCheck("Feet", 1);
+            int actual = feet.ConvertFeetToInch();
+            Assert.AreNotEqual(1, actual);
         }
 
         [Test]
         public void Compare_1inch_Not_Equal_to_1ft_return_False()
         {
-            UnitCheck feet = new UnitCheck("Feet", 1);
-            int actual = feet.ConvertFeetToInch();
+            UnitCheck inch = new UnitCheck("Inch", 1);
+            double actual = inch.ConvertInchToFeet();
             Assert.AreNotEqual(1, actual);
         }
+
+        [Test]
+        public void Compare_1Feet_Equal_to_12_inch_return_True()
+        {
+            UnitCheck feet = new UnitCheck("Feet", 1);
+            int actual = feet.ConvertFeetToInch();
+            Assert.AreEqual(12,actual);
+        }
+
+        [Test]
+        public void Compare_12_inch_Equal_to_1_Feet_return_True()
+        {
+            UnitCheck inch = new UnitCheck("Inch", 12);
+            double actual = inch.ConvertInchToFeet();
+            Assert.AreEqual(1, actual);
+        }
+
+
+
     }
 }
