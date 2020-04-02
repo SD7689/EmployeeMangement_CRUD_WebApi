4abced07f90a33403ee1060673fbc0f8d2998571
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-02 12:04:17 +0530

    [Shivam] Add. TC1.1 created a ConvertToFeet Method to return equal

---
+++ QualityMeasurement/Feet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace QualityMeasurement
+{
+    public class Feet 
+    {
+        public int feet;
+
+        public Feet(int feet)
+        {
+            this.feet = feet;
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    if (feet.Equals(obj))
+        //        return true;
+        //    else
+        //        return false;
+
+        //}
+
+        public override bool Equals(object obj)
+        {
+            if (this.GetType().Equals(obj.GetType()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ConvertFeetValue(Feet feet)
+        {
+            if (this.feet.Equals(feet.feet))
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+}
