9f507aac7c6c74661e2c7da6e9465292df479e81
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-03 16:52:09 +0530

    [Shivam] Add. Created a Celcius Class

---
+++ QualityMeasurement/Celcius.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Celcius.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QualityMeasurement
+{
+    using System;
+
+    /// <summary>
+    /// Celcius Class.
+    /// </summary>
+    public class Celcius
+    {
+        /// <summary>
+        /// int celcius property.
+        /// </summary>
+        public int celcius;
+
+        /// <summary>
+        /// celcius Constructor.
+        /// </summary>
+        public Celcius()
+        {
+        }
+
+        /// <summary>
+        /// celcius Constructor with parameter.
+        /// </summary>
+        /// <param name="celcius"></param>
+        public Celcius(int celcius)
+        {
+            this.celcius = celcius;
+        }
+
+        /// <summary>
+        /// EqualsValue Method.
+        /// </summary>
+        /// <returns> int </returns>
+        public int EqualsValue()
+        {
+            return this.celcius;
+        }
+
+        /// <summary>
+        /// Equals Method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns> bool </returns>
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
