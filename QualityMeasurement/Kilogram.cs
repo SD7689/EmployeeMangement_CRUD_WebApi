784e1884d7eab1277404cb5a101bc7b660fe5cc7
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-03 16:53:13 +0530

    [Shivam] Add. Created a Kilogram Class

---
+++ QualityMeasurement/Kilogram.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Kilogram.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QualityMeasurement
+{
+    using System;
+
+    /// <summary>
+    /// Kilogram Class.
+    /// </summary>
+    public class Kilogram
+    {
+        /// <summary>
+        /// int Kilogram property.
+        /// </summary>
+        public int kg;
+
+        /// <summary>
+        /// Kilogram Constructor.
+        /// </summary>
+        public Kilogram()
+        {
+        }
+
+        /// <summary>
+        /// Kilogram Constructor with parameter.
+        /// </summary>
+        /// <param name="kg"></param>
+        public Kilogram(int kg)
+        {
+            this.kg = kg;
+        }
+
+        /// <summary>
+        /// EqualsValue Method.
+        /// </summary>
+        /// <returns> int </returns>
+        public int EqualsValue()
+        {
+            return this.kg;
+        }
+
+        /// <summary>
+        /// Equals Method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns> bool </returns>
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
