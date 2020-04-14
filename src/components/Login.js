14899887a6d6039b33bfd62d0eb63191d9ae34a8
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-14 14:35:16 +0530

    [Shivam] Add. Creatd a FUnction for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,43 @@ import {Container,Button,Col,Row,Form} from 'react-bootstrap';
 import './loginCss.css';
 
 export class Login extends Component {
+constructor(props){
+    super(props)
+    this.state = {
+        email:"",
+        password: ""
+    }
+}
+
+handleEmailChange=(event)=>{
+    const email=event.target.value;
+    this.setState({
+        email:email
+    })
+    console.log("email",this.state.email)
+}
+
+handleEmailChange=(event)=>{
+    const email=event.target.value;
+    this.setState({
+        email:email
+    })
+    console.log("email",this.state.email)
+}
+
+handlePasswordChange=(event)=>{
+    const password=event.target.value;
+    this.setState({
+        password:password
+    })
+    console.log("password",this.state.password)
+}
+
+handleSubmitButton=()=>{
+    console.log("status", this.state.email, this.state.password);
+}
+
+
 
     render() {
         return (
@@ -14,11 +51,10 @@ export class Login extends Component {
             <h3 className = "m-4 d-flex justify-content-center">
             Login Page
             </h3>
-            <Form.Group controlId="validationCustom01"></Form.Group>
 
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" onChange={this.handleEmailChange}/>
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
@@ -26,14 +62,14 @@ export class Login extends Component {
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" onChange={this.handlePasswordChange}/>
             </Form.Group>
 
             <Form.Group controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={this.handleSubmitButton} >
                 Submit
             </Button>
                 <p className="forgot-password text-right">
