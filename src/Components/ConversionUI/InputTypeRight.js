4be8c15ece7f93d88ec68830d19590db62fd9463
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-21 13:27:22 +0530

    [Shivam] Add. created a InputTypeRight DropDown component for UI

---
+++ src/Components/ConversionUI/InputTypeRight.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+class InputTypeRight extends Component{
+    render(){
+        return(
+            <div className='col-md m-5' id='main-label'>
+                <input type='text' className="form-control form-control-md" onChange={this.props.inputTypeRight} value={this.props.valueRight} />
+                <select name="measurementType" id="measurementType" className='form-control'  onChange={this.props.selectTypeRight}>
+                <option value="-1" selected>Select</option>
+                {
+                    this.props.showType?
+                    <>
+                <option name="length">Feet</option>
+                <option name="weight">Inch</option>
+                <option name="weight">Meter</option>
+                <option name="weight">Centimeter</option></> : <>
+                <option name="weight">Kilogram</option>
+                <option name="weight">Gram</option>
+                </>
+                }
+                </select>
+            </div>
+        )
+    }
+}
+export default InputTypeRight;
