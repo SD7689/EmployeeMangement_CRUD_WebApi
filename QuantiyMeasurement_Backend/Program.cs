1ea7bef6fbd5c3250e338c9b3e8eb43fbb5b84aa
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-04-29 12:46:38 +0530

    [Shivam] Refactor. Implemented Receive Queue in program.cs and write it to path.

--- QuantiyMeasurement_Backend/Program.cs
+++ QuantiyMeasurement_Backend/Program.cs
@@ -7,10 +7,12 @@
 
 namespace QuantiyMeasurement_Backend
 {
-
+    using Experimental.System.Messaging;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using QuantityMeasurement_Backend;
+    using System;
+    using System.IO;
 
     /// <summary>
     /// Program Class.
@@ -19,8 +21,37 @@ namespace QuantiyMeasurement_Backend
     {
         public static void Main(string[] args)
         {
+            MessageQueue MyQueue = null;
+            string path = @".\private$\Send";
+            try
+            {
+
+                MyQueue = new MessageQueue(path);
+                Message[] messages = MyQueue.GetAllMessages();
+                if (messages.Length > 0)
+                {
+                    foreach (Message msg in messages)
+                    {
+                        msg.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+                        string message = msg.Body.ToString();
+                        MyQueue.Receive();
+                        File.WriteAllText(@"C:\Users\Shiv\source\repos\QuantiyMeasurement_Backend\msmqReciver.txt", message);
+                    }
+                    MyQueue.Refresh();
+                }
+                else
+                {
+                    Console.WriteLine("No New Messages in Message Queue");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             BuildWebHost(args).Run();
         }
+        
         public static IWebHost BuildWebHost(string[] args) =>
                 WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
