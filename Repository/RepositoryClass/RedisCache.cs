67d9706d0a1afe7db48590cdc054543f4d9e2dea
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-05-02 21:00:10 +0530

    [Shivam] Add. Created a RedisCAche Class to Get and Set KeyValuePairs

---
+++ Repository/RepositoryClass/RedisCache.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RedisCache.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Repository.RepositoryClass
+{
+    using StackExchange.Redis;
+
+    /// <summary>
+    /// RedisCache Class.
+    /// </summary>
+    public class RedisCache
+    {
+
+        /// <summary>
+        /// RedisConnection with parameter.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string RedisConnection(string key)
+        {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            IDatabase data = redis.GetDatabase();
+
+            //  data.StringSet(key, value);
+            string keyValue = data.StringGet(key);
+            return keyValue;
+        }
+
+        /// <summary>
+        /// RedisConnection with 2 parameter.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void RedisConnection(string key, string value)
+        {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            IDatabase data = redis.GetDatabase();
+
+            data.StringSet(key, value);
+            data.StringGet(key);
+        }
+    }
+}
