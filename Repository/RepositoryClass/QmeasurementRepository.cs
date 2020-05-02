02a5c4a28f4a2e2bf37dba82114b1f9a1122861a
dewanganshivam89@gmail.com <dewanganshivam89@gmail.com>
2020-05-02 23:04:40 +0530

    [Shivam] Refactor. Implemented method for redis cache

--- Repository/RepositoryClass/QmeasurementRepository.cs
+++ Repository/RepositoryClass/QmeasurementRepository.cs
@@ -8,6 +8,7 @@
 namespace Repository
 {
     using Model;
+    using Repository.RepositoryClass;
 
     /// <summary>
     /// QmeasurementRepository implements IQmeasurementRepository.
@@ -16,13 +17,20 @@ namespace Repository
     {
 
         /// <summary>
+        /// RedisCache class object created.
+        /// </summary>
+        RedisCache redisCache = new RedisCache();
+
+        /// <summary>
         /// FeettoInch method.
         /// </summary>
         /// <param name="value"></param>
         /// <returns> value</returns>
         public decimal FeettoInch(Qmeasurement value)
         {
-            return value.Feet * 12; 
+            decimal result = value.Feet * 12;
+            redisCache.RedisConnection($"Feet", result.ToString());
+            return result;
         }
 
         /// <summary>
@@ -32,17 +40,9 @@ namespace Repository
         /// <returns> value</returns>
         public decimal InchToFeet(Qmeasurement value)
         {
-            return value.Inch / 12;
-        }
-
-        /// <summary>
-        /// GmtoKg method.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns> value</returns>
-        public decimal GmtoKg(Qmeasurement value)
-        {
-            return value.Gram / 1000;
+            decimal result = value.Inch / 12;
+            redisCache.RedisConnection($"Inch", result.ToString());
+            return result;
         }
 
         /// <summary>
@@ -52,7 +52,9 @@ namespace Repository
         /// <returns> value</returns>
         public decimal MetertoCentimeter(Qmeasurement value)
         {
-            return value.Meter * 100;
+            decimal result = value.Meter * 100;
+            redisCache.RedisConnection($"Meter", result.ToString());
+            return result;
         }
 
         /// <summary>
@@ -62,7 +64,21 @@ namespace Repository
         /// <returns> value</returns>
         public decimal CentimetertoMeter(Qmeasurement value)
         {
-            return value.Centimeter / 100;
+            decimal result = value.Centimeter / 100;
+            redisCache.RedisConnection($"Centimeter", result.ToString());
+            return result;
+        }
+
+        /// <summary>
+        /// GmtoKg method.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns> value</returns>
+        public decimal GmtoKg(Qmeasurement value)
+        {
+            decimal result = value.Gram / 1000;
+            redisCache.RedisConnection($"Gram", result.ToString());
+            return result;
         }
 
         /// <summary>
@@ -72,7 +88,9 @@ namespace Repository
         /// <returns> value</returns>
         public decimal KgtoGm(Qmeasurement value)
         {
-            return value.KiloGram * 1000;
+            decimal result = value.KiloGram * 1000;
+            redisCache.RedisConnection($"KiloGram", result.ToString());
+            return result;
         }
     }
 }
